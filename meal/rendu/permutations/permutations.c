7264002eee9b4b0f5ed2ee61af7d1fa84b51232b
Eugenio Quintas <equintas@c2r7s9.42luanda.com>
2026-01-28 10:37:38 +0100

    Adding

--- meal/rendu/permutations/permutations.c
+++ meal/rendu/permutations/permutations.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/*/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   permutations.c                                     :+:      :+:    :+:   */
@@ -15,22 +15,21 @@
 #include <string.h>
 #include <unistd.h>
 
-void	ft_swap(char *a, char *b)
+int	ft_strlen(char *str)
 {
-	char	c;
-
-	c = *a;
-	*a = *b;
-	*b = c;
+	int i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-void	char_sort(char *str, int len)
+void	char_sort(char *str,int start, int len)
 {
 	int		i;
 	int		j;
 	char	tmp;
 
-	i = 0;
+	i = start;
 	while (i < len - 1)
 	{
 		j = i + 1;
@@ -51,30 +50,55 @@ void	char_sort(char *str, int len)
 void	permutations(char *str, int start, int end)
 {
 	int	i;
+	int j;
+	char tmp;
 
 	if (start == end)
 	{
-		write(1, str, end + 1);
+		write(1, str, ft_strlen(str));
 		write(1, "\n", 1);
 		return ;
 	}
 	i = start;
-	while (i <= end)
+	while (i <= end) 
 	{
-		ft_swap(&str[start], &str[i]);
+		tmp = str[i];
+		j = i;
+		while (j > start)
+		{
+			str[j] = str[j - 1];
+			j--;
+		}
+		str[start] = tmp;
 		permutations(str, start + 1, end);
-		ft_swap(&str[start], &str[i]);
+		j = start;
+		while (j < i)
+		{
+			str[j] = str[j + 1];
+			j++;
+		}
+		str[i] = tmp;
 		i++;
-	}
+	}	
 }
 
 int	main(int ac, char *av[])
 {
-	(void)ac;
-	(void)av;
-	if (ac == 2)
-	{   
-        char_sort(av[1], strlen(av[1]));
-		permutations(av[1], 0, strlen(av[1]) - 1);
+	if (ac != 2 || !av[1][0])
+		return (1);
+	int len = ft_strlen(av[1]);
+	char *str = malloc (len + 1);
+	if (!str)
+		return (0);
+	int i = 0;
+	while (i < len)
+	{
+		str[i] = av[1][i];
+		i++;
 	}
+	str[i] = 0;
+	char_sort(str, 0, len);
+	permutations(str, 0, len - 1);
+	free (str);
+	return (0);
 }
